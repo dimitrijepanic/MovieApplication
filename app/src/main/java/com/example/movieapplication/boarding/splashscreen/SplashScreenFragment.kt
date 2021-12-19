813b58e1228db6745a215bc283dfe92c2976aafc
Internship <mitadimitrije.panic@gmail.com>
2021-12-19 17:17:35 +0100

    Added App Icon and Splash Screen Image

--- app/src/main/java/com/example/movieapplication/boarding/splashscreen/SplashScreenFragment.kt
+++ app/src/main/java/com/example/movieapplication/boarding/splashscreen/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.movieapplication.boarding.splashscreen
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.transition.TransitionInflater
+import com.example.movieapplication.R
 import com.example.movieapplication.databinding.FragmentSplashScreenBinding
 import com.example.movieapplication.main.MainActivity
 import kotlinx.coroutines.delay
@@ -17,30 +20,34 @@ class SplashScreenFragment : Fragment() {
 
     private lateinit var binding: FragmentSplashScreenBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val inflater = TransitionInflater.from(requireContext())
+        exitTransition = inflater.inflateTransition(R.transition.fade)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
-
         return binding.root
     }
-
-
 
     init {
         startMainActivity()
     }
 
-    private fun startMainActivity(){
-        lifecycleScope.launch{
+    private fun startMainActivity() {
+        lifecycleScope.launch {
             delay(2000)
             sendIntent()
         }
     }
 
-    private suspend fun sendIntent(){
-        startActivity(Intent(this.requireContext(), MainActivity::class.java))
+    private suspend fun sendIntent() {
+        startActivity(Intent(this.requireContext(), MainActivity::class.java),
+            ActivityOptions.makeSceneTransitionAnimation(requireActivity()).toBundle())
     }
 }
