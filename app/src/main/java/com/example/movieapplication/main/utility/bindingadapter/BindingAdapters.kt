41bb23138e5ae717069d1f3ba66f0bd4515d4b06
Internship <mitadimitrije.panic@gmail.com>
2021-12-21 17:16:16 +0100

    Added Layouts and Binding Adapters For Genres Fragment

---
+++ app/src/main/java/com/example/movieapplication/main/utility/bindingadapter/BindingAdapters.kt
@@ -0,0 +1,40 @@
+package com.example.movieapplication.main
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.movieapplication.main.network.GenreProperty
+import com.example.movieapplication.main.network.MovieProperty
+import com.example.movieapplication.main.utility.adapter.GenreGridAdapter
+import com.example.movieapplication.main.utility.adapter.MovieGridAdapter
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val fullImagePath = "https://image.tmdb.org/t/p/w500/$imgUrl"
+        val imgUri = fullImagePath.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
+    }
+}
+
+@BindingAdapter("movieData")
+fun bindMovieData(
+    recyclerView: RecyclerView,
+    data: List<MovieProperty>?
+) {
+    val adapter = recyclerView.adapter as MovieGridAdapter
+    adapter.submitList(data)
+}
+
+@BindingAdapter("genreData")
+fun bindGenreData(
+    recyclerView: RecyclerView,
+    data: List<GenreProperty>?
+) {
+    val adapter = recyclerView.adapter as GenreGridAdapter
+    adapter.submitList(data)
+}
